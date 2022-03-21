6e3b9dd668ffa6c5a6c3dadb64d0761a7cbee246
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-22 00:06:14 +1300

    [feat]: Set up Canvas.
    Set up Canvas. Stars are just for checking orbit controls.

--- solar_system/src/App.js
+++ solar_system/src/App.js
@@ -1,7 +1,19 @@
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from "react";
+import { OrbitControls, Stars } from '@react-three/drei'
 
 function App() {
+
   return (
-    <></>
+    <Canvas>
+      <Suspense fallback={null}>
+        <pointLight position={[10, 10, 10]} />
+        <ambientLight intensity={0.4} />
+        <directionalLight />
+        <OrbitControls />
+        <Stars />
+      </Suspense>
+    </Canvas>
   );
 }
 
