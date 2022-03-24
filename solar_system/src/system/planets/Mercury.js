0e73cbbf5db1257d9d9966af1c5f8ad7086b0493
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-24 16:38:28 +1300

    [feat]: Implemented planet surfaces.
    Implemented planet surfaces - added surface textures.

--- solar_system/src/system/planets/Mercury.js
+++ solar_system/src/system/planets/Mercury.js
@@ -1,11 +1,16 @@
 import { NoRingPlanet } from "./template/NoRingPlanet";
 import { sizeMercury, distSunMercury } from "../values/measurements";
+import MercurySurface from "./surfaces/mercury.jpg"
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 export const Mercury = ({ sunRef }) => {
+    
+    const texture = useLoader(TextureLoader, MercurySurface);
 
     return (
         <>
-            <NoRingPlanet sunRef={sunRef} distanceFromSun={distSunMercury} radius={sizeMercury} />
+            <NoRingPlanet sunRef={sunRef} distanceFromSun={distSunMercury} radius={sizeMercury} texture={texture} />
         </>
     )
 
