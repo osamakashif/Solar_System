380afccc9ee98b1a1ac7c51ce4fbcad035b52981
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-03-24 16:00:29 +1300

    [feat]: Radius of the Sun.
    Added prop radius of the Sun.

--- solar_system/src/system/sun/Sun.js
+++ solar_system/src/system/sun/Sun.js
@@ -1,16 +1,15 @@
 import { useFrame, useLoader } from "@react-three/fiber";
-import { useEffect } from "react";
 import {TextureLoader} from "three"
 import SunSurface from "./surfaces/sun.jpg"
 
-export const Sun = ({sunRef}) => {
+export const Sun = ({sunRef, radius=20}) => {
     const texture = useLoader(TextureLoader, SunSurface);
     useFrame(()=>{
         sunRef.current.rotation.y = sunRef.current.rotation.y+0.002
     })
     return (
         <mesh ref={sunRef} position={[0, 0, 0]}>
-            <sphereBufferGeometry />
+            <sphereBufferGeometry args={[radius]} />
             <meshBasicMaterial attach="material" map={texture} />
         </mesh>
     )
